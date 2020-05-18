c395f2f9c2ba262224fc292d65165f625fa0e497
krai29 <karan29rai@gmail.com>
2020-05-18 23:12:23 +0530

    Initial commit

---
+++ app/src/main/java/com/krai29/groceryorderapp/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.krai29.groceryorderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
---
+++ app/src/main/java/com/krai29/groceryorderapp/ShoppingDao.kt
@@ -0,0 +1,21 @@
+package com.krai29.groceryorderapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems():LiveData<List<ShoppingItem>>
+
+
+
+
+}
